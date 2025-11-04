44cca63858984479c0c75e9b67734af3d362e226
Harshit Yadav <harshityadav6704@gmail.com>
2025-11-04 21:29:16 +0530

    Connect Messages with promises

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 dotenv.config({
     path: './env'
 }
 )
 
-
 connectDB()
+.then(()=>{
+    
+    const server= app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running on port : ${process.env.PORT}`);
+        
+    });
+    server.on ("error", (err) => {
+      console.error("Server error:", err);
+      process.exit(1);
+    });
+}
+)
+.catch((err)=>{
+    console.log("MongoDB Connection failed",err);
+    
+})
