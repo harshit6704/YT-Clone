0308c88cbb362314944dad0a65101dacd5d45e5a
Harshit Yadav <harshityadav6704@gmail.com>
2025-11-05 15:18:54 +0530

    Utils (asyncHandler,APIerror Message, API Success Response)

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(stausCode,data,message="Success"){
+        this.stausCode=stausCode
+        this.data=data
+        this.message=message
+        this.success=stausCode<400
+    }
+}
+export {ApiResponse}
